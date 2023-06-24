448fa4a8655ce86b1cc7554b6654eed05e0ac295
ehakan <ehakangencoglu@gmail.com>
2023-06-24 13:18:22 +0300

    feat(gitops): add base argocd and bootstrapping requirements

--- kubernetes/clusters/kind-mbp-intel-macos/extras/bootstrap-secrets.zsh
+++ kubernetes/clusters/kind-mbp-intel-macos/extras/bootstrap-secrets.zsh
@@ -14,12 +14,12 @@ jwt=$(op read op://homelab/homelab-1password-jwt/credential)
 echo "=== [003/006] Reading '1password-credentials.json' ==="
 credentials=$(op read op://homelab/homelab-1password-credentials/1password-credentials.json | base64)
 
-echo "=== [004/006] Creating secret 'onepassword-connect-token' ==="
-kubectl create secret generic --namespace onepassword onepassword-connect-token \
+echo "=== [004/006] Creating secret 'op-token' ==="
+kubectl create secret generic --namespace onepassword op-token \
   --from-literal=token="$jwt"
 
 echo "=== [005/006] Creating secret 'connect-server-credentials' ==="
-kubectl create secret generic --namespace onepassword connect-server-credentials \
+kubectl create secret generic --namespace onepassword op-credentials \
   --from-literal=1password-credentials.json="${credentials}"
 
 echo "=== [006/006] Secret Bootstrapping complete, continue on with the rest of the bootstrapping process."
